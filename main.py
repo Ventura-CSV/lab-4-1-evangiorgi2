abcbd510f24a9a11a5b0ade998f27adde28344be
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 12:54:50 -0700

    Power of 2 List Complete

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ def main():
 
     N = int(input('Enter the number N: '))
     result = []
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    for i in range(0, N + 1):
+        power = 2 ** i
+        result.append(power)
+    
+    print (result)
     return result
 
 
